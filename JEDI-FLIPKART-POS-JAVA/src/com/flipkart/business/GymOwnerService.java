a50fb92890bb7834db5d7b54dfaec6f91626b9be
sodu2301-hehe <iamkumarsourav6203@GMAIL.COM>
2024-06-26 07:54:46 +0530

    Update GymOwnerService.java

--- JEDI-FLIPKART-POS-JAVA/src/com/flipkart/business/GymOwnerService.java
+++ JEDI-FLIPKART-POS-JAVA/src/com/flipkart/business/GymOwnerService.java
@@ -1,32 +1,20 @@
 package com.flipkart.business;
 
-import com.flipkart.DAO.GymOwnerDAO;
 import com.flipkart.bean.GymOwner;
 
-import java.util.List;
-
-public class GymOwnerService implements GymOwnerServiceInterface {
-    private static GymOwnerDAO gymOwnerDAO = new GymOwnerDAO();
-    public void requestGymOwnerApproval(String gymOwnerId) {
-        gymOwnerDAO.sendOwnerApprovalRequest(gymOwnerId);
-
-    }
-
-    public List<GymOwner> viewAllGymOwners() {
-        return gymOwnerDAO.getGymOwnerList();
-    }
-
-
-//    public Object addGymOwnerDetails() {
-//        //takes gymOwner object as input
-//        return new Object();
-//    }
-
+public class GymOwnerService {
+    
     public boolean loginGymOwner(String username,String password){
-        return gymOwnerDAO.loginGymOwner(username,password);
+        // if login details is valid
+        return true;
     }
 
-    public void registerGymOwner(String userId,String userName, String password, String email, String panNumber,String cardNumber) {
-        gymOwnerDAO.registerGymOwner(new GymOwner(userId,userName,email,password,panNumber,cardNumber));
+    public void registerGymOwner(String userId,String userName, String password, String email, String panNumber,String gstNumber) {
+          System.out.println("Registration Done Successfully");
+    }
+
+     public void requestGymOwnerApproval(String gymOwnerId) {
+        System.out.println("Approval requested");
+
     }
 }
